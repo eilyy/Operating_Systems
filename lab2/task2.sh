4ee069bcde25aad3344a525cac0099877bc4a904
user <user@localhost.localdomain>
2020-11-02 21:15:47 +0300

    dumb issues fixed

--- lab2/task2.sh
+++ lab2/task2.sh
@@ -1,3 +1,3 @@
-#$/bin/bash
+#!/bin/bash
 
 ps -Ao pid,command | grep -h "[0-9] /sbin/" | awk '{print $1}' > task2_result
--- lab3/task2.sh
+++ lab3/task2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-at now + 2min -f ./task1.sh
+at now + 1min -f ./task1.sh
 
 tail -n 0 -f ~/report
-
-exit 0
